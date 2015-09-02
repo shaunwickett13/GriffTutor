78db86c1351d3d4852b4ecffbe7a12f4d73ba47f
Shaun Wickett <wicketts@brahe.canisius.edu>
2015-09-02 17:20:41 -0400

    updated script to create admin user

--- createAdminUser.php
+++ createAdminUser.php
@@ -2,23 +2,38 @@
 
 require_once __DIR__.'/oop/manager/Database.php';
 require_once __DIR__.'/oop/Security.php';
+require_once __DIR__.'/oop/manager/UserManager.php';
 
-echo "Enter info for new admin user";
-echo "____________________________________"
-$ID = readline("Enter ID: ");
+echo "\nENTER INFO FOR NEW ADMIN USER\n";
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
 
-$name = readline("Enter name: ");
+$ID = readline("ID: ");
 
-$PIN = readline("Enter 4 digit pin: ");
+$name = readline("Name: ");
 
-$hashedPW = Security::obtainHash($PIN, $ID);
+$PIN = readline("4 digit pin: ");
+
+$hashedPIN = Security::obtainHash($PIN, $ID);
+echo 'hashed PIN is '. $hashedPIN;
 
 $query = "INSERT INTO `User` (`userID`, `PIN`, `name`, `role`) VALUES
-($ID, $PIN, $name, 'supervisor');";
+($ID, `$hashedPIN`, `$name`, `supervisor`);";
 
+echo "\ncreating DB connection\n";
 $db = new Database();
-$db -> $this->query($query);
 
-echo "\nAdmin user created!";
+echo "running query\n";
+$db -> query($query);
 
+$um = new UserManager();
+
+$success = $um -> userExists($ID);
+
+if ($success) {
+	echo "\nadmin user created";
+}
+else {
+	echo "\nfailed to create user";
+}
+echo '';
 ?>
