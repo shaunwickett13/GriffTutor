0e79b673b31abef87af626ba481ba3d3cbbe7634
shaunwickett13 <shaunwickett13@gmail.com>
2015-09-02 17:07:12 -0400

    updated database setup script

--- setDatabase.php
+++ setDatabase.php
@@ -2,67 +2,66 @@
 
 $query = "
 
-DROP TABLE IF EXISTS `Schedule`, `Timesheet`, `Tutor`, `User`, 'Report';
+DROP TABLE IF EXISTS 'Schedule', 'Timesheet', 'Tutor', 'User', 'Report';
 
-CREATE TABLE `Schedule` (
-  `userID` int(11) NOT NULL,
-  `day` varchar(10) NOT NULL,
-  `hour` int(4) NOT NULL
+CREATE TABLE 'Schedule' (
+  'userID' int(11) NOT NULL,
+  'day' varchar(10) NOT NULL,
+  'hour' int(4) NOT NULL
+);
+
+ALTER TABLE 'Schedule'
+ ADD PRIMARY KEY ('userID','day','hour');
+
+
+CREATE TABLE 'Timesheet' (
+'timesheetID' int(11) NOT NULL,
+  'userID' int(11) NOT NULL,
+  'dateCompleted' date NOT NULL,
+  'filename' varchar(30) NOT NULL
+);
+
+ALTER TABLE 'Timesheet'
+ ADD PRIMARY KEY ('timesheetID');
+
+ ALTER TABLE 'Timesheet'
+ MODIFY 'timesheetID' int(11) NOT NULL AUTO_INCREMENT;
+
+
+CREATE TABLE 'Tutor' (
+  'userID' int(11) NOT NULL,
+  'email' varchar(30) NOT NULL,
+  'phone' varchar(10) NOT NULL,
+  'bio' text NOT NULL,
+  'picture' varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `Schedule`
- ADD PRIMARY KEY (`userID`,`day`,`hour`);
+ALTER TABLE 'Tutor'
+ ADD PRIMARY KEY ('userID');
 
+CREATE TABLE 'User' (
+'userID' int(11) NOT NULL,
+  'PIN' varchar(40),
+  'name' varchar(30) NOT NULL,
+  'role' set('supervisor','tutor') NOT NULL
+);
 
-CREATE TABLE `Timesheet` (
-`timesheetID` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `dateCompleted` date NOT NULL,
-  `filename` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE 'User'
+ ADD PRIMARY KEY ('userID');
 
-ALTER TABLE `Timesheet`
- ADD PRIMARY KEY (`timesheetID`);
+ALTER TABLE 'User'
+ MODIFY 'userID' int(11) NOT NULL AUTO_INCREMENT;
 
- ALTER TABLE `Timesheet`
- MODIFY `timesheetID` int(11) NOT NULL AUTO_INCREMENT;
-
-
-CREATE TABLE `Tutor` (
-  `userID` int(11) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `phone` varchar(10) NOT NULL,
-  `bio` text NOT NULL,
-  `picture` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-ALTER TABLE `Tutor`
- ADD PRIMARY KEY (`userID`);
-
-
-CREATE TABLE `User` (
-`userID` int(11) NOT NULL,
-  `PIN` varchar(40) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `role` set('supervisor','tutor') NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
-ALTER TABLE `User`
- ADD PRIMARY KEY (`userID`);
-
-ALTER TABLE `User`
- MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
-
-INSERT INTO `User` (`userID`, `PIN`, `name`, `role`) VALUES
+INSERT INTO 'User' ('userID', 'PIN', 'name', 'role') VALUES
 (0, '9ba7a7240f4e591d55f173e83faa73d7a92aeeec', 'Admin', 'supervisor');
 
 
 CREATE TABLE 'Report' (
-'userID' int(11) NOT NULL,
-'date' date NOT NULL,
-'studentName' varchar(30) NOT NULL,
-'report' text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  'userID' int(11) NOT NULL,
+  'date' date NOT NULL,
+  'studentName' varchar(30) NOT NULL,
+  'report' text NOT NULL
+);
 
 ALTER TABLE 'Report'
  ADD PRIMARY KEY ('userID');
